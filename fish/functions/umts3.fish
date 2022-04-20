cc9526b83118a2f44977c5e4ead4ea2363bfdd05
Ferit Yiğit BALABAN <fyb@duck.com>
2022-04-20 12:42:52 +0300

    dotman 20.04.2022 12.42

---
+++ fish/functions/umts3.fish
@@ -0,0 +1,3 @@
+function umts3 --wraps='umount /home/ferit/mounts/01_AmazonS3' --description 'alias umts3 umount /home/ferit/mounts/01_AmazonS3'
+  umount /home/ferit/mounts/01_AmazonS3 $argv; 
+end
